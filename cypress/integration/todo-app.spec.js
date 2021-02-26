76a7be317f72e467026afc109aae97f07bb30359
yasmany <yasmanys88@gmail.com>
2021-02-25 22:50:34 -0300

    usando beforeEach

--- cypress/integration/todo-app.spec.js
+++ cypress/integration/todo-app.spec.js
@@ -1,8 +1,19 @@
 /// <reference types="cypress"/>
 describe('Todo application', () => {
-    it('should log any accessibility failures', () => {
+    beforeEach('', () => {
         cy.visit('http://todomvc.com/examples/react');
         cy.injectAxe();
+    });
+
+    it('should log any accessibility failures', () => {
         cy.checkA11y();
+    });
+
+    it('should exclude specific elements on the page', () => {
+        cy.checkA11y({ exclude: ['.learn'] });
+    });
+
+    it('should only test specific elements on the page', () => {
+        cy.checkA11y('.learn');
     });
 });
